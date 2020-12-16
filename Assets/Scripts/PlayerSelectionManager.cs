af3fce76b68f61d07e0c111079c80eec57ca2442
shayne243 <marques0@purdue.edu>
2020-12-16 11:51:20 +0400

    Rotation in player selection manager

---
+++ Assets/Scripts/PlayerSelectionManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerSelectionManager : MonoBehaviour
+{
+    public Transform playerSwitcherTransform;
+
+    public Button next_Button;
+    public Button previous_Button;
+
+    // Start is called before the first frame update
+    #region UNITY Methods
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    #endregion
+
+    #region UI Callback Methods
+    public void NextPlayer()
+    {
+        next_Button.enabled = false;
+        previous_Button.enabled = false;
+        StartCoroutine(Rotate(Vector3.up, playerSwitcherTransform, 90, 1.0f));
+    }
+
+    public void PreviousPlayer()
+    {
+        next_Button.enabled = false;
+        previous_Button.enabled = false;
+        StartCoroutine(Rotate(Vector3.up, playerSwitcherTransform, -90, 1.0f));
+    }
+    #endregion
+
+    #region Private Methods
+    IEnumerator Rotate(Vector3 axis, Transform transformToRotate, float angle, float duration = 1.0f)
+    {
+        Quaternion originalRotation = transformToRotate.rotation;
+        Quaternion finalRotation = transformToRotate.rotation * Quaternion.Euler(axis * angle);
+        float elapsedTime = 0.0f;
+        while (elapsedTime < duration)
+        {
+            transformToRotate.rotation = Quaternion.Slerp(originalRotation, finalRotation, elapsedTime/duration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        transformToRotate.rotation = finalRotation;
+
+        next_Button.enabled = true;
+        previous_Button.enabled = true;
+
+    }
+    #endregion
+}
