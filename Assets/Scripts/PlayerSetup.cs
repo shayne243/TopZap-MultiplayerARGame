c62c7b867a41f9c8159c816139fe4b013e259335
shayne243 <marques0@purdue.edu>
2020-12-16 14:09:10 +0400

    Added player names and local plaayer objects

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class PlayerSetup : MonoBehaviourPun
+{
+
+    public TextMeshProUGUI playerNameText;
+
+    void Start()
+    {
+        if (photonView.IsMine)
+        {
+            transform.GetComponent<MovementController>().enabled = true;
+            transform.GetComponent<MovementController>().joystick.gameObject.SetActive(true);
+        }
+        else
+        {
+            transform.GetComponent<MovementController>().enabled = false;
+            transform.GetComponent<MovementController>().joystick.gameObject.SetActive(false);
+        }
+        SetPlayerName();
+    }
+
+    void SetPlayerName()
+    {
+        if (playerNameText != null)
+        {
+            if (photonView.IsMine)
+            {
+                playerNameText.text = "YOU";
+                playerNameText.color = Color.red;
+            }
+            else
+            {
+                playerNameText.text = photonView.Owner.NickName;
+            }
+        }
+    }
+}
