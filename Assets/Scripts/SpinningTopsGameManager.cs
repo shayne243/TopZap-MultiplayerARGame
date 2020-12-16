958e73dd3fee4de59e9a61b4b330e8100169c66a
shayne243 <marques0@purdue.edu>
2020-12-16 13:16:32 +0400

    Joining Rooms added

---
+++ Assets/Scripts/SpinningTopsGameManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+
+public class SpinningTopsGameManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    #region UI Callback Methods
+    public void JoinRandomRoom()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    #endregion
+
+
+    #region PHOTON Callback Methods
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log(message);
+        CreateAndJoinRoom();
+    }
+
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log(" joined to " + PhotonNetwork.CurrentRoom.Name);
+    }
+
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log(newPlayer.NickName + " joined to " + PhotonNetwork.CurrentRoom.Name + " Player count " + PhotonNetwork.CurrentRoom.PlayerCount);
+    }
+
+
+    #endregion
+
+
+    #region PRIVATE Methods
+    void CreateAndJoinRoom()
+    {
+        string randomRoomName = "Room" + Random.Range(0, 1000);
+
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 2;
+
+        PhotonNetwork.CreateRoom(randomRoomName, roomOptions);
+    }
+    #endregion
+}
+
